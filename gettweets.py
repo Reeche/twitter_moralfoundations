4d20d3b2df2c6e4b24021cd59b3d3d52fbb7a801
Reeche <ruiqi.h92@googlemail.com>
2019-05-08 15:09:03 +0100

    Update gettweets.py

--- gettweets.py
+++ gettweets.py
@@ -5,7 +5,6 @@ import re
 import tweepy
 import csv
 import asyncio
-import timeit
 
 
 auth = tweepy.OAuthHandler("1LkXJ9SqzQm7an39boqQ5AW0v",
@@ -14,42 +13,13 @@ auth.set_access_token("911681697577611264-lQDqG7lcZGjvL1vIvrR3qKTlaPZH3Xp",
                       "AZqWtegYKM7GrsN0AVmwaqMHQH72R85lzaEwQvpflam8x")
 
 # set wait_on_rate_limit = True to wait automatically
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify = True, retry_count=10, retry_delay = 5)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify = True, retry_count=5, retry_delay = 5)
 
-# set number of pages
-users = [api.get_user("CDU", page = x) for x in range(51, 101)]
-
-
-follower = []
-names = []
-for user in users:
-    print("get users")
-    for friend in user.followers():
-        #print("friend.id")
-        follower.append(friend.id)
-        names.append(friend.name)
+# set number of pages, get a list of followers
+users = tweepy.Cursor(api.followers, screen_name="CDU").items(5000)
 
 
-#
-# print(api.user_timeline(911681697577611264))
-#
-# file = open("testfile.json", "w")
-# temp = []
-# for id in follower:
-#     try:
-#         # print("start printing id", id)
-#         # print(api.user_timeline(id))
-#         temp.append(api.user_timeline(id))
-#     except:
-#         print("error")
-# print(temp)
-# print(type(temp))
-# file.write(temp)
-# file.close()
-
-
-async def get_tweets(ids, party):
-    await asyncio.sleep(1)
+def get_tweets(ids, party):
     print(ids)
     # initialize a list to hold all the tweepy Tweets
     alltweets = []
@@ -63,7 +33,6 @@ async def get_tweets(ids, party):
     except tweepy.TweepError:
         print("Failed to run the command on that user ", ids, " , Skipping...")
 
-    # transform the tweepy tweets into a 2D array that will populate the csv	| you can comment out data you don't need
     outtweets = [[ids,
                 #tweet.id_str,
                   tweet.created_at,
@@ -84,7 +53,7 @@ async def get_tweets(ids, party):
 
 if __name__ == '__main__':
 
-    party = "CDU2"
+    party = "CDUall"
     with open('%s_tweets.csv' % party, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(["id",
@@ -95,11 +64,11 @@ if __name__ == '__main__':
                          "retweeted",
                          #"source",
                          "text"])
+    n = 0
+    for user in users:
+        n += 1
+        get_tweets(user.id, party)
+        print(n)
 
-    loop = asyncio.get_event_loop()
-    for ids in follower:
-        loop.run_until_complete(get_tweets(ids, party))
-
-    loop.close()
 
 
